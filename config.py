232a353545bc0766360d30b408c9c791650d3199
Saher Pervaiz <126251612+03066207138@users.noreply.github.com>
2025-06-14 20:33:06 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,7 @@
-<<<<<<< HEAD
+
 PAGE_TITLE = "📦 Supply Chain Dashboard"
 HEADER_TITLE = "📦Supply Chain Calendar"
 SAMPLE_SCHEDULE = "sample_data/supply_chain_schedule.json"
 SAMPLE_VRP = "sample_data/vrp_actions.json"
-=======
-PAGE_TITLE = "📦 Supply Chain Dashboard"
-HEADER_TITLE = "📦Supply Chain Calendar"
-SAMPLE_SCHEDULE = "sample_data/supply_chain_schedule.json"
-SAMPLE_VRP = "sample_data/vrp_actions.json"
->>>>>>> da4307a7d4ca3e7e27b1ef19d08ebf19ef615f79
+
+
